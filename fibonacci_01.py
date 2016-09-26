01e090460fab4ddf38c8419203c692ddd75316cc
Peter Luptak <peterluptak@MacBook-Air.local>
2016-09-26 05:50:42 +0200

    added recursive algo which avoids exponential running time through dynamic programming

--- fibonacci_01.py
+++ fibonacci_01.py
@@ -1,6 +1,23 @@
 
 
-'''return n-th number in the Fibonacci sequence'''
+'''
+return n-th number in the Fibonacci sequence
+
+Recursive algo performance (in seconds):
+n=30 : 0.2
+n=33 : 1.0
+n=36 : 3.7
+n=39 : 15.9
+
+Dynamic programming algo performance (in seconds):
+n=30 : 0.0
+n=33 : 0.0
+n=36 : 0.0
+n=39 : 0.0
+n=100 : 0.0
+n=500 : 0.0
+
+'''
 
 def fib(n):
 	
@@ -10,6 +27,16 @@ def fib(n):
 		return fib(n-1) + fib(n-2)
 
 
+def dp_fib(n):
+	if n in lib.keys():
+		return lib[n]
+	else :
+		a = dp_fib(n-1) + dp_fib(n-2)
+		lib[n] = a
+		#print(lib)
+		return a
 
 
-print fib(40)
+
+lib = {1:1, 2:1}
+print dp_fib(500)
